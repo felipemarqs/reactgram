4d7a8f32d67a3f538cbabb99273550c33d307069
Felipe Marques <felipe.marqs@outlook.com>
2022-12-28 18:57:38 -0300

    att

--- frontend/src/pages/Profile/index.jsx
+++ frontend/src/pages/Profile/index.jsx
@@ -1,60 +1,82 @@
-import { Container, Header, HeaderDetails } from "./styles"
+import { Container, Header, HeaderDetails } from "./styles";
 
-
-import { uploads } from "../../utils/config"
+import { uploads } from "../../utils/config";
 
 //components
 
-import { Message } from "../../components/Message"
-import { Link } from "react-router-dom"
-import { BsFillEyeFill, BsPencilFill, BsXLg } from "react-icons/bs"
+import { Message } from "../../components/Message";
+import { Link } from "react-router-dom";
+import { BsFillEyeFill, BsPencilFill, BsXLg } from "react-icons/bs";
 
 //hooks
-import { useState , useEffect , useRef } from "react"
-import { useSelector, useDispatch } from "react-redux"
-import { useParams } from "react-router-dom"
+import { useState, useEffect, useRef } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
 
 //redux
-import { getUserDetails } from "../../slices/userSlice"
-
-
-
+import { getUserDetails } from "../../slices/userSlice";
 
 export const Profile = () => {
+  const { id } = useParams();
 
-    const { id } = useParams()
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch()
+  const { user, loading } = useSelector((state) => state.user);
+  const { user: userAuth } = useSelector((state) => state.auth);
 
-    const { user , loading } = useSelector((state) => state.user)
-    const { user : userAuth } = useSelector((state) => state.auth)
+  //Photo
+  //New form and edit form refs
+  const newPhotoForm = useRef()
+  const editPhotoForm = useRef()
 
-    //Photo
+  //Load user data
+  useEffect(() => {
+    dispatch(getUserDetails(id));
+  }, [dispatch, id]);
 
-    //Load user data
-    useEffect(() => {
-        dispatch(getUserDetails(id))
-    }, [dispatch, id])
+  const submitHandle = (e) => {
+    e.preventDefault();
+    window.alert("send")
+  }
 
-    if (loading) {
-        return <p>Carregando...</p>
-    }
+  if (loading) {
+    return <p>Carregando...</p>;
+  }
 
-    return (
-        <Container>
-            <Header>
-                {user.profileImage && (
-                    <img src={`${uploads}/users/${user.profileImage}`} alt="" />
-                )}
+  return (
+    <Container>
+      <Header>
+        {user.profileImage && (
+          <img src={`${uploads}/users/${user.profileImage}`} alt="" />
+        )}
 
-                <HeaderDetails>
-                    <h2>{user.name}</h2>
-                    <p>{user.bio}</p>
-                </HeaderDetails>
+        <HeaderDetails>
+          <h2>{user.name}</h2>
+          <p>{user.bio}</p>
+        </HeaderDetails>
+      </Header>
 
-                   
+      {id === userAuth._id && (
+        <>
+          <div ref={newPhotoForm}>
+                <h3>Compartilhe alguma photo</h3>
 
-            </Header>
-        </Container>
-    )
-}
+                <form onSubmit={submitHandle}>
+                    <label>
+                        <span>Título para foto:</span>
+                        <input type="text" placeholder="Insira um título"></input>
+                    </label>
+
+                    <label>
+                        <span>Imagem:</span>
+                        <input type="file" />
+                    </label>
+
+                    <input type="submit" value="Enviar" />
+                </form>
+          </div>
+        </>
+      )}
+    </Container>
+  );
+};
