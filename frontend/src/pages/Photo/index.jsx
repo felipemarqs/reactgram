a679a703441e97dc3c03375af1c6be9bddefeb7b
Felipe Marques <felipe.marqs@outlook.com>
2023-01-03 19:54:10 -0300

    atrt

--- frontend/src/pages/Photo/index.jsx
+++ frontend/src/pages/Photo/index.jsx
@@ -2,17 +2,44 @@ import React from "react";
 
 import { uploads } from "../../utils/config";
 
+//Components
+import { Message } from "../../components/Message";
+import { Link, useParams } from "react-router-dom";
+
+//Hooks
+import { useEffect, useState } from "react";
+
+//redux
+import { useSelector, useDispatch } from "react-redux";
+import { getPhoto } from "../../slices/photoSlice";
+
 //styles
-import { Container} from './styles'
+import { Container } from "./styles";
 
 export const Photo = () => {
+  const { id } = useParams();
 
-    return (
-        <Container>
-            Photo Page 
-        </Container>
-    )
+  const dispatch = useDispatch();
 
+  const { user } = useSelector((state) => state.auth);
 
+  const { photo, loading, error, message } = useSelector(
+    (state) => state.photo
+  );
 
+    //Load photo data
+
+    useEffect(() => {
+        dispatch(getPhoto(id))
+    }, [dispatch, id])
+
+    //Like and comment
+
+    if ( loading ) {
+        return <p>Carregando...</p>
+    }
+    
+  return <Container>Photo Page</Container>;
 };
+
+
